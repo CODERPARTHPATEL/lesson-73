61be30391054a8cb1cb660c5bdea5fa4e81e0ab5
CODERPARTHPATEL <nskp.patel09@gmail.com>
2026-05-01 19:40:53 +0100

    lesson 73

--- IntroToPandas.py
+++ IntroToPandas.py
@@ -3,7 +3,7 @@ import numpy as np
 
 exam_data = {'name':['anastasia','dima','katherone','james','emily',
 'michael','laura','kevin','jonas','mathew'],
-            'score':[12.5,9,9,16.5,np.nan,9,20,14.5,np.nan,8,19],
+            'score':[12.5,9,9,16.5,np.nan,9,20,14.5,np.nan,8],
             'attempts':[1,3,2,3,2,3,1,1,2,1],
             'qualify':['yes','no','yes','no','yes','yes','no','no','yes','no']}
 
