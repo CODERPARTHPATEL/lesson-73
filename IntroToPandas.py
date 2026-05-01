f421bbc8096c924ed90ef2ec1d66670530927b76
CODERPARTHPATEL <nskp.patel09@gmail.com>
2026-05-01 19:38:09 +0100

    lesson 73

---
+++ IntroToPandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+exam_data = {'name':['anastasia','dima','katherone','james','emily',
+'michael','laura','kevin','jonas','mathew'],
+            'score':[12.5,9,9,16.5,np.nan,9,20,14.5,np.nan,8,19],
+            'attempts':[1,3,2,3,2,3,1,1,2,1],
+            'qualify':['yes','no','yes','no','yes','yes','no','no','yes','no']}
+
+lable = ['a','b','c','d','e','f','g','h','i','j']
+
+df = pd.DataFrame(exam_data , index=lable)
+print('summary of the basics')
+print(df.info())
